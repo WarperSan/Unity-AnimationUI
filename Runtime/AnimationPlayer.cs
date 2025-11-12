61c3778c8c845264b903ee2d1b84f0561207a7bc
WarperSan <leumas.ecole@gmail.com>
2025-11-12 15:02:14 -0500

    Added basic player

---
+++ Runtime/AnimationPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using AnimationUI.Sequences;
+using UnityEngine;
+
+namespace AnimationUI
+{
+    public class AnimationPlayer : MonoBehaviour
+    {
+        [SerializeReference]
+        public Sequence[] sequences = {};
+
+        private void Start()
+        {
+            StartCoroutine(Play());
+        }
+
+        private IEnumerator Play()
+        {
+            foreach (var sequence in sequences)
+            {
+                yield return sequence.Play();
+            }
+        }
+    }
+}
